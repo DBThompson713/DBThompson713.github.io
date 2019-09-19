b146384888a5aa0e7ab8c94ff4044eb78203cdde
DBThompson713 <dbthompson.au@gmail.com>
2019-09-19 14:33:48 +1000

    jquery for shader class fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $("#one").click(function() {
+    $(this).hasClass("swingout")
+      ? $(".shader").removeClass("shaded")
+      : $(".shader").addClass("shaded");
+
     $(this).toggleClass("swingout");
     $(".link-fade").toggleClass("main-link-fade");
     $(".first-link").toggleClass("first-fade");
     $(".second-link").toggleClass("second-fade");
-    $(".shader").toggleClass("shaded");
     // TWO
     $("#two").removeClass("swingout");
     $(".link-fade-two").removeClass("main-link-fade");
@@ -20,12 +23,15 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("#two").click(function() {
+    $(this).hasClass("swingout")
+      ? $(".shader").removeClass("shaded")
+      : $(".shader").addClass("shaded");
+
     $(this).toggleClass("swingout");
     $(".link-fade-two").toggleClass("main-link-fade");
     $(".first-link-two").toggleClass("first-fade");
     $(".second-link-two").toggleClass("second-fade");
     $(".third-link-two").toggleClass("third-fade");
-    $(".shader").toggleClass("shaded");
     // ONE
     $("#one").removeClass("swingout");
     $(".link-fade").removeClass("main-link-fade");
@@ -40,10 +46,13 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("#three").click(function() {
+    $(this).hasClass("swingout")
+      ? $(".shader").removeClass("shaded")
+      : $(".shader").addClass("shaded");
+
     $(this).toggleClass("swingout");
     $(".first-link-three").toggleClass("main-link-fade");
     $(".first-link-three").toggleClass("first-fade");
-    $(".shader").toggleClass("shaded");
     // ONE
     $("#one").removeClass("swingout");
     $(".link-fade").removeClass("main-link-fade");
@@ -60,29 +69,29 @@ $(document).ready(function() {
 
 // start of transition
 
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: "zoom-in-lg",
-    outClass: "zoom-out-lg",
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: ".animsition-link",
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: "body", //animsition wrapper element
-    loadingClass: "animsition-loading",
-    loadingInner: "", // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: ["animation-duration", "-webkit-animation-duration"],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay: false,
-    overlayClass: "animsition-overlay-slide",
-    overlayParentElement: "body",
-    transition: function(url) {
-      window.location.href = url;
-    }
-  });
-});
+// $(document).ready(function() {
+//   $(".animsition").animsition({
+//     inClass: "zoom-in-lg",
+//     outClass: "zoom-out-lg",
+//     inDuration: 1500,
+//     outDuration: 800,
+//     linkElement: ".animsition-link",
+//     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+//     loading: true,
+//     loadingParentElement: "body", //animsition wrapper element
+//     loadingClass: "animsition-loading",
+//     loadingInner: "", // e.g '<img src="loading.svg" />'
+//     timeout: false,
+//     timeoutCountdown: 5000,
+//     onLoadEvent: true,
+//     browser: ["animation-duration", "-webkit-animation-duration"],
+//     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+//     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+//     overlay: false,
+//     overlayClass: "animsition-overlay-slide",
+//     overlayParentElement: "body",
+//     transition: function(url) {
+//       window.location.href = url;
+//     }
+//   });
+// });
