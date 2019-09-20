4b3f1e539748588ccaf913f02ba1bf605a455fad
DBThompson713 <dbthompson.au@gmail.com>
2019-09-20 13:21:38 +1000

    changed .bio to .info and linked to main page bio

--- scripts/main.js
+++ scripts/main.js
@@ -98,3 +98,33 @@ $(document).ready(function() {
     $(".first-link-three").removeClass("first-fade");
   });
 });
+
+$(document).ready(function() {
+  $(".info-click").click(function() {
+    $("#one").hasClass("swingout")
+      ? $(".shader").removeClass("shaded")
+      : $(".shader").addClass("shaded");
+
+    $("#name").toggleClass("name-fly");
+    $("#tag").toggleClass("tag-fly");
+    $(".main-page-bio").toggleClass("main-page-bio-fadein");
+    $(".swinger").toggleClass("dissapear");
+    $(".techs").toggleClass("techs-fadein");
+    // $(".first-link-three").toggleClass("first-fade");
+    // ONE
+    $("#one").removeClass("swingout");
+    $(".link-fade").removeClass("main-link-fade");
+    $(".first-link").removeClass("first-fade");
+    $(".second-link").removeClass("second-fade");
+    // TWO
+    $("#two").removeClass("swingout");
+    $(".link-fade-two").removeClass("main-link-fade");
+    $(".first-link-two").removeClass("first-fade");
+    $(".second-link-two").removeClass("second-fade");
+    $(".third-link-two").removeClass("third-fade");
+    // THREE
+    $("#three").removeClass("swingout");
+    $(".first-link-three").removeClass("main-link-fade");
+    $(".first-link-three").removeClass("first-fade");
+  });
+});
