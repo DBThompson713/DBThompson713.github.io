0c714974df43be99833d3576c23c98d80387035a
Dale Thompson <dalethompson@192-168-1-111.tpgi.com.au>
2019-10-03 18:58:15 +1000

    update to sydney, attempt at sending contact form data to google sheets

---
+++ scripts/formHandle.js
@@ -0,0 +1,16 @@
+var $form = $("form#test-form"),
+  url =
+    "https://script.google.com/macros/s/AKfycbx_XFWIdyG-7ZXCSYbYdMB7exHTQhe2k8zwLysI2H9DoC_dbOw/exec";
+
+$("#submit-form").on("click", function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  })
+    .success
+    // do something
+    ();
+});
