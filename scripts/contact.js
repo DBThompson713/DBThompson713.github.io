ab74aadbe5394e843efe11723fb893ca881b90ff
Dale Thompson <dbthompson.au@gmail.com>
2019-10-08 11:28:05 +1100

    changed contact final input to textarea, updated styles and cleaned up contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,22 +3,6 @@ function postToGoogle() {
   var field2 = $("#emailField").val();
   var field3 = $("#commentField").val();
 
-  if (field1 == "") {
-    alert("Please Fill Your Name");
-    document.getElementById("nameField").focus();
-    return false;
-  }
-  if (field2 == "") {
-    alert("Please Fill Your Email");
-    document.getElementById("emailField").focus();
-    return false;
-  }
-  if (field3 == "") {
-    alert("How's it going?");
-    document.getElementById("commentField").focus();
-    return false;
-  }
-
   $.ajax({
     url:
       "https://docs.google.com/forms/d/e/1FAIpQLSfmJKK6uWpjCUI31QxEqh5I4PH0h4HY9mTO9Qtkx5-B7GQ9eQ/formResponse?",
